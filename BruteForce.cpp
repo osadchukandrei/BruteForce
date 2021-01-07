95b10e37708e6eddc205ab5ed3dbcf53206a7650
osadchukandrei <mukomol44@gmail.com>
2021-01-07 22:09:55 +0200

    2Level

--- BruteForce.cpp
+++ BruteForce.cpp
@@ -10,8 +10,10 @@
 
 unsigned char key[EVP_MAX_KEY_LENGTH];
 unsigned char iv[EVP_MAX_IV_LENGTH];
-std::vector<unsigned char> HashEn;
+
 std::vector<unsigned char> HashDe;
+std::vector<unsigned char> HashEn;
+std::string pathFile;
 
 void ReadFile(const std::string& filePath, std::vector<unsigned char>& buf)
 {
@@ -20,7 +22,6 @@ void ReadFile(const std::string& filePath, std::vector<unsigned char>& buf)
     {
         throw std::runtime_error("Can not open file " + filePath);
     }
-
     buf.clear();
     buf.insert(buf.begin(), std::istreambuf_iterator<unsigned char>(fileStream), std::istreambuf_iterator<unsigned char>());
     fileStream.close();
@@ -33,11 +34,15 @@ void WriteFile(const std::string& filePath, const std::vector<unsigned char>& bu
     fileStream.close();
 }
 
-void AppendToFile(const std::string& filePath, const std::vector<unsigned char>& buf)
+void AppendToFile(const std::string& filePath, const std::string& passw)
 {
-    std::basic_ofstream<unsigned char> fileStream(filePath, std::ios::binary | std::ios::app);
-    fileStream.write(&buf[0], buf.size());
+    std::fstream fileStream(filePath, std::ios::binary | std::ios::app);
+    fileStream.write(&passw[0], passw.size());
     fileStream.close();
+}
+
+void CreateFile(const std::string& filePath) {
+    std::basic_ofstream<unsigned char> fileStream(filePath, std::ios::binary);
 }
 
 void PasswordToKey(std::string& password)
@@ -47,7 +52,6 @@ void PasswordToKey(std::string& password)
     {
         throw std::runtime_error("no such digest");
     }
-
     const unsigned char* salt = NULL;
     if (!EVP_BytesToKey(EVP_aes_128_cbc(), EVP_md5(), salt,
         reinterpret_cast<unsigned char*>(&password[0]),
@@ -57,69 +61,25 @@ void PasswordToKey(std::string& password)
     }
 }
 
-void EncryptAes(const std::vector<unsigned char> plainText, std::vector<unsigned char>& chipherText)
-{
-    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-    if (!EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
-    {
-        throw std::runtime_error("EncryptInit error");
-    }
-
-    std::vector<unsigned char> chipherTextBuf(plainText.size() + AES_BLOCK_SIZE);
-    int chipherTextSize = 0;
-    if (!EVP_EncryptUpdate(ctx, &chipherTextBuf[0], &chipherTextSize, &plainText[0], plainText.size())) {
-        EVP_CIPHER_CTX_free(ctx);
-        throw std::runtime_error("Encrypt error");
-    }
-
-    int lastPartLen = 0;
-    if (!EVP_EncryptFinal_ex(ctx, &chipherTextBuf[0] + chipherTextSize, &lastPartLen)) {       
-        throw std::runtime_error("EncryptFinal error");
-    }
-    chipherTextSize += lastPartLen;
-    chipherTextBuf.erase(chipherTextBuf.begin() + chipherTextSize, chipherTextBuf.end());
-
-    chipherText.swap(chipherTextBuf);
-
-    EVP_CIPHER_CTX_free(ctx);
-}
-
 void CalculateHash(const std::vector<unsigned char>& data, std::vector<unsigned char>& hash)
 {
     std::vector<unsigned char> hashTmp(SHA256_DIGEST_LENGTH);
-
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, &data[0], data.size());
     SHA256_Final(&hashTmp[0], &sha256);
-
     hash.swap(hashTmp);
 }
 
-void Encrypt()
-{
-    std::vector<unsigned char> plainTextEn;
-    ReadFile("plain_text.txt", plainTextEn);
-    
-    CalculateHash(plainTextEn, HashEn);    
-
-    std::vector<unsigned char> chipherTextEn;
-    EncryptAes(plainTextEn, chipherTextEn);
-
-    WriteFile("chipher_text.txt", chipherTextEn);
-
-    AppendToFile("chipher_text.txt", HashEn);
-}
-
-void DecryptAes(const std::vector<unsigned char> chipherText, std::vector<unsigned char> &plainText) { 
+void DecryptAes(const std::vector<unsigned char> chipherText, std::vector<unsigned char>& plainText) {
     EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
 
     if (!EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
     {
         throw std::runtime_error("DecryptInit error");
     }
-    
-    std::vector<unsigned char> plainTextBuf(chipherText.size() );
+
+    std::vector<unsigned char> plainTextBuf(chipherText.size());
     int plainTextSize = 0;
     if (!EVP_DecryptUpdate(ctx, &plainTextBuf[0], &plainTextSize, &chipherText[0], chipherText.size())) {
         EVP_CIPHER_CTX_free(ctx);
@@ -127,7 +87,7 @@ void DecryptAes(const std::vector<unsigned char> chipherText, std::vector<unsign
     }
 
     int lastPartLen = 0;
-    if (!EVP_DecryptFinal_ex(ctx, &plainTextBuf[0] + plainTextSize, &lastPartLen)) { 
+    if (!EVP_DecryptFinal_ex(ctx, &plainTextBuf[0] + plainTextSize, &lastPartLen)) {
         EVP_CIPHER_CTX_free(ctx);
         throw std::runtime_error("DecryptFinal error");
     }
@@ -135,46 +95,57 @@ void DecryptAes(const std::vector<unsigned char> chipherText, std::vector<unsign
     plainTextBuf.erase(plainTextBuf.begin() + plainTextSize, plainTextBuf.end());
 
     plainText.swap(plainTextBuf);
-
     EVP_CIPHER_CTX_free(ctx);
-
 }
 
-void CompareHashes(std::vector<unsigned char> &hashDe, std::vector<unsigned char> &hashEn) {
-    if (!(hashDe == hashEn)) {
-        throw std::runtime_error("Hashes not equal");;
+void CompareHashes(std::vector<unsigned char>& hashDe, std::vector<unsigned char>& hashEn) {
+    if (hashDe != hashEn) {
+        throw std::runtime_error("Hashes not equal");
     }
 }
 
-void DetachmentHash(std::vector<unsigned char> & chipherText,size_t sizeHash) {
+void DetachmentHash(std::vector<unsigned char>& chipherText, size_t sizeHash) {
+    HashEn = chipherText;
+    HashEn.erase(HashEn.begin(), HashEn.begin() + HashEn.size() - sizeHash);//hash
     size_t newSizeChipherText = chipherText.size() - sizeHash;
-    chipherText.erase(chipherText.begin()+newSizeChipherText,chipherText.end());
+    chipherText.erase(chipherText.begin() + newSizeChipherText, chipherText.end());
 }
 
-void Decrypt() {
-    std::vector<unsigned char> chipherTextDe;
-    ReadFile("chipher_text.txt", chipherTextDe);
-
-    DetachmentHash(chipherTextDe, HashEn.size());//detachment hash from chipher text.
+void Decrypt(std::string password, std::vector<unsigned char>& chipherBuf) {
 
     std::vector<unsigned char> plainTextDe;
-    DecryptAes(chipherTextDe, plainTextDe);
+    DecryptAes(chipherBuf, plainTextDe);
 
-    CalculateHash(plainTextDe,HashDe);// hash decrypt text.
-    CompareHashes(HashDe,HashEn);
-
+    AppendToFile("logPasswords.txt", password);
     WriteFile("plain_text111.txt", plainTextDe);
 }
 
-int main()
-{
-    OpenSSL_add_all_digests();   
-    std::string pass = "pass";
+void OpenChipherFile(int argc, char* argv[]) {
+    switch (argc) {
+    case 2:
+        pathFile = argv[1];
+        break;
+    case 3:
+        pathFile = argv[1];
+        CreateFile("logPasswords.txt");
+        break;
+    default:
+        throw std::runtime_error("not parameters");
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    OpenSSL_add_all_digests();
+    std::string pass = "pass";    
+    std::vector<unsigned char> chipherText;
     try
     {
+        OpenChipherFile(argc, argv);
+        ReadFile(pathFile, chipherText);
         PasswordToKey(pass);
-        Encrypt();
-        Decrypt();
+        DetachmentHash(chipherText, HashEn.size());
+        Decrypt(pass, chipherText);
     }
     catch (const std::runtime_error& ex)
     {
